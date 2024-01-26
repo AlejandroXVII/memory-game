a6d10ebc6f955388c9daa767f389482bc29a3f2d
AlejandroXVII <m.alejandro.xvii@gmail.com>
2024-01-25 21:45:49 -0400

    Add function to render the cards and change places

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import { getArrayPokemonIDs, shuffleArray } from "./pokemonList";
 
+function PokemonCards() {
+	const [pokemonIDs, setPokemonIDs] = useState([5, 1, 2, 3, 4]);
+	function shufflePokemon(e) {
+		e.preventDefault();
+		setPokemonIDs([...shuffleArray(pokemonIDs)]);
+	}
+	return (
+		<div>
+			{pokemonIDs.map((pokemon) => (
+				<button onClick={shufflePokemon} key={pokemon}>
+					{pokemon}
+				</button>
+			))}
+		</div>
+	);
+}
 function App() {
 	const [img, setIMG] = useState(0);
 	const [pokeName, setPokeName] = useState(0);
 	const [id, setID] = useState(1);
+	function getRandomInt(max) {
+		return Math.floor(Math.random() * max);
+	}
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -12,7 +32,6 @@ function App() {
 				"https://pokeapi.co/api/v2/pokemon/" + id
 			);
 			const pokemonData = await response.json();
-			console.log(pokemonData);
 			setIMG(pokemonData.sprites);
 			setPokeName(pokemonData.name);
 		};
@@ -36,8 +55,10 @@ function App() {
 				<button onClick={() => setID(getRandomInt(1000))}>
 					picachu yo te elijo!
 				</button>
+				<button onClick={() => getArrayPokemonIDs(5)}>get IDs</button>
 			</div>
 			<p className="read-the-docs">Click to get your pokemon</p>
+			<PokemonCards />
 		</>
 	);
 }
